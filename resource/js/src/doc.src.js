890b61dbcf471f16d2b2e18833d1d3d5b806d74b
Jimmy Shiau <jimmyshiau73@gmail.com>
2012-07-13 15:16:01 +0800

    Fixed: remove the replace url javascript in the doc page

--- resource/js/src/doc.src.js
+++ resource/js/src/doc.src.js
@@ -1,6 +1,6 @@
 $(function() {
 	var path = window.location.href;
-	
+	path = path.substring(path.indexOf('/rikulo/latest/'), path.length);
 	$('#toc a').each(function() {
 		var $n = $(this);
 		if ($n.attr('href') == path) {
