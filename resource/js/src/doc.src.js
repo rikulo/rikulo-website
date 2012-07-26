4900b1fd08976ff5d0dfebe5c2b1a884904f42ee
Jimmy Shiau <jimmyshiau73@gmail.com>
2012-07-26 19:12:12 +0800

    Fixed doc page: select highlight

--- resource/js/src/doc.src.js
+++ resource/js/src/doc.src.js
@@ -1,6 +1,8 @@
 $(function() {
 	var path = window.location.href;
 	path = path.substring(path.indexOf('/rikulo/latest/'), path.length);
+	if (path.charAt(path.length-1) == '/')
+		path = path.substring(0, path.length - 1);
 	$('#toc a').each(function() {
 		var $n = $(this);
 		if ($n.attr('href') == path) {
