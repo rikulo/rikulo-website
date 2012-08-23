7163e38694fe05bea6c159c565e1adb1ff768774
Jimmy Shiau <jimmyshiau73@gmail.com>
2012-08-23 15:21:09 +0800

    apply links to twitter area

--- resource/js/src/index.src.js
+++ resource/js/src/index.src.js
@@ -1,9 +1,30 @@
 $(function() {
 	$('.twitters').each(function(index) {
-		var $n = $(this);
+		var $n = $(this),
+			$auther = $n.find('.author'),
+			userNameUrl = 'https://twitter.com/intent/user?screen_name=' + $auther.html(),
+			$saying = $n.find('.saying'),
+			sayingCnt = $saying.html(),
+			profileTemplate = 'https://twitter.com/#!/search/?q=%23',
+			annoTemplate = 'https://twitter.com/';
+		
+		/*add align CSS*/
 		$n.children('.pointer').addClass((index % 2 == 0)? 'pointer-r': 'pointer-l');
-		$n.children('.twitter-img').addClass((index % 2 == 0)? 'img-r': 'img-l');
+		$n.find('a .twitter-img').addClass((index % 2 == 0)? 'img-r': 'img-l');
 		$n.children('.bubble').addClass((index % 2 == 0)? 'bubble-l': 'bubble-r');
+		
+		/*apply links*/
+		$auther.attr('href', userNameUrl);
+		$n.find('.img-icon').attr('href', userNameUrl);
+		
+		sayingCnt = sayingCnt.replace(/(http[s]?:\/\/(.*?\S*))/g, 
+			'<a href="$1">$2</a>');
+		
+		sayingCnt = sayingCnt.replace(/(@(.*?\S*))/g, 
+			'<a href="'+annoTemplate+'$2">$1</a>');
+		
+		$saying.html(sayingCnt.replace(/(#(.*?\S*))/g, 
+			'<a href="'+profileTemplate+'$2&src=hash">$1</a>'));
 	});
 	
 	if (isTouchDevice()) {
@@ -15,10 +36,13 @@ $(function() {
 		},function() {
 			$(this).children().first().fadeTo(200, 0);
 		});
-		$(".twitters. .bubble-cnt").hover(function() {
+		$(".twitters .bubble-cnt").hover(function() {
 			$(this).addClass('edit');
 		},function() {
 			$(this).removeClass('edit');
 		});
 	}
+	$(".twitters").each(function() {
+		
+	});
 });
